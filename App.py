32238e92dd15b397562079816ed935823c954912
Jake248Newman <jake248newman@gmail.com>
2025-11-29 14:42:47 +0000

    template app.py

--- App.py
+++ App.py
@@ -1,1 +1,27 @@
-print("Hello World")
+#import firebase_admin
+#from firebase_admin import credentials, firestore
+from flask import Flask, jsonify
+from flask_cors import CORS
+import datetime
+
+from Ship import Ship
+from CrewMember import CrewMember
+
+app = Flask(__name__)
+CORS(app)
+
+"""
+#Firebase setup
+cred = credentials.Certificate("certs/firebase-key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+"""
+
+ship = Ship()
+
+@app.route("/data/telemetry", methods=["GET"])
+def get_telemetry():
+    pass
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
