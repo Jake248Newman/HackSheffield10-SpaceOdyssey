b462ec9a8af4f59fbae8fff48ec8a70770926c11
Jake248Newman <jake248newman@gmail.com>
2025-11-29 17:47:47 +0000

    updated crew member

--- CrewMember.py
+++ CrewMember.py
@@ -1,28 +1,52 @@
 class CrewMember:
-    def __init__(self, name, age, gender, occupation):
+    def __init__(self, name, job):
         self.name = name
-        self.age = age
-        self.gender = gender
-        self.occupation = occupation
         self.health = 100
+        self.sanity = 100
+        self.status = "Alive"
+        self.hunger = 100
+        self.job = job
 
     def get_name(self):
         return self.name
-    def get_age(self):
-        return self.age
-    def get_gender(self):
-        return self.gender
-    def get_occupation(self):
-        return self.occupation
+
+    def set_name(self, name):
+        self.name = name
+
     def get_health(self):
-        return self.health
+        return str(self.health) + "%"
+
     def set_health(self, health):
         self.health = health
+
+    def get_sanity(self):
+        return str(self.sanity) + "%"
+
+    def set_sanity(self, sanity):
+        self.sanity = sanity
+
+    def get_status(self):
+        return self.status
+
+    def set_status(self, status):
+        self.status = status
+
+    def get_hunger(self):
+        return str(self.hunger) + "%"
+
+    def set_hunger(self, hunger):
+        self.hunger = hunger
+
+    def get_job(self):
+        return self.job
+
+    def set_job(self, job):
+        self.job = job
+
+    #todo update this
     def to_dict(self):
         return {
             "name": self.name,
-            "age": self.age,
-            "gender": self.gender,
-            "occupation": self.occupation,
+            "occupation": self.job,
             "health": self.health
         }
